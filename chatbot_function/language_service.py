6e8f1b07b80f0e8ba0f69b522632bd244403cbef
ITU-0327 <itunghsieh@gmail.com>
2024-02-15 04:58:52 +1100

    Add documentation for detect_language()

--- chatbot_function/language_service.py
+++ chatbot_function/language_service.py
@@ -1,16 +1,15 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.translation.document import DocumentTranslationClient, TranslationTarget
-from .utils import get_secret
 import os
 
+from .utils import get_secret
 
-# Initialise Azure Cognitive Services credentials
-endpoint = os.environ["AZURE_LANGUAGE_SERVICE_ENDPOINT"]
+
 language_service_key = get_secret('language-service-key')
+endpoint = os.environ["AZURE_LANGUAGE_SERVICE_ENDPOINT"]
 
 
-# Function to create a Text Analytics Client
 def authenticate_client():
     ta_credential = AzureKeyCredential(language_service_key)
     text_analytics_client = TextAnalyticsClient(
@@ -18,19 +17,38 @@ def authenticate_client():
             credential=ta_credential)
     return text_analytics_client
 
-def detect_language(input_text):
+
+def detect_language(input_text: str) -> str:
+    """
+    Detects the primary language of the given input text using Azure's Text Analytics API.
+
+    If the input text is empty or contains only whitespace, the function returns 'und' to
+    indicate an undefined language, as making a call with empty content could be wasteful
+    or lead to unnecessary processing.
+
+    Args:
+        input_text (str): The text for which to detect the language.
+
+    Returns:
+        str: The ISO 639-1 language code of the detected primary language, or 'und' if the
+        input text is empty or cannot be processed.
+    """
+    if not input_text.strip():
+        return 'und'
+
     client = authenticate_client()
-    response = client.detect_language(documents=[{"id": "1", "text": input_text}])
-    
-    if response is not None and len(response) > 0:
-        for document in response:
-            if 'primary_language' in document:
-                language = document['primary_language']['iso6391_name']
-                return(language)
-            else:
-                return("No language detected.")
-    else:
-        return("No language detected.")
+
+    try:
+        response = client.detect_language(documents=[{"id": "1", "text": input_text}])
+        if response is not None and len(response.documents) > 0:
+            document = response.documents[0]
+            if document.detected_languages:
+                primary_language = document.detected_languages[0]
+                return primary_language.iso6391_name
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return 'und'
 
 
 # Function to translate text
