04e37183bd68afe7d85c8b07ee0305c0413acafc
shuenyng <stan0175@student.monash.edu>
2024-02-15 00:38:53 +1100

    Added language detection

--- chatbot_function/language_service.py
+++ chatbot_function/language_service.py
@@ -3,19 +3,13 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.translation.document import DocumentTranslationClient, TranslationTarget
 import os
 
-subscription_key = "b0850fb413f048169de90fff4584b65a"
-endpoint = "YOUR_TRANSLATOR_ENDPOINT"
-
-
 
 # Initialise Azure Cognitive Services credentials
 # key = os.environ["AZURE_LANGUAGE_SERVICE_KEY"]
 # endpoint = os.environ["AZURE_LANGUAGE_SERVICE_ENDPOINT"]
-key = "b0850fb413f048169de90fff4584b65a"
-endpoint = "https://cba-translator.cognitiveservices.azure.com/"
+key = "0e28e50d295144e0811dd4c36aabf286"
+endpoint = "https://cba-languageservice.cognitiveservices.azure.com/"
 
-credential = AzureKeyCredential(subscription_key)
-translator_client = TextAnalyticsClient(endpoint, credential)
 
 # Function to create a Text Analytics Client
 def authenticate_client():
@@ -25,13 +19,19 @@ def authenticate_client():
             credential=ta_credential)
     return text_analytics_client
 
-# Function to detect language
 def detect_language(input_text):
     client = authenticate_client()
     response = client.detect_language(documents=[{"id": "1", "text": input_text}])
-    if response is not None and len(response.documents) > 0:
-        return response.documents[0].primary_language.iso6391_name
-    return "en"  # Default to English if detection fails
+    
+    if response is not None and len(response) > 0:
+        for document in response:
+            if 'primary_language' in document:
+                language = document['primary_language']['iso6391_name']
+                print(f"Detected Language: {language}")
+            else:
+                print("No language detected.")
+    else:
+        print("No language detected.")
 
 # Test input text samples
 input_texts = [
@@ -47,13 +47,13 @@ input_texts = [
 
 # Call the detect_language function for each input text
 for text in input_texts:
-    detected_language = detect_language(text)
-    print(f"Input Text: '{text}' | Detected Language: {detected_language}")
+    detect_language(text)
 
 # Test an edge case with an empty string
 empty_text = ""
 detected_language_empty = detect_language(empty_text)
 print(f"Empty Text | Detected Language: {detected_language_empty}")
+
 
 
 # Function to translate text
